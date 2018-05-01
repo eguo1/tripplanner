3f48cb1d1798716c6365b546be18ddf2d811de66
Eric Guo <ericxguo@gmail.com>
2018-05-01 15:06:01 -0500

    got the map working!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('reached src/index.js')
+const mapboxgl = require('mapbox-gl')
+
+mapboxgl.accessToken = "pk.eyJ1IjoiZWd1bzEiLCJhIjoiY2pnbzMyeTNnMGhkcTJxbnkyYmFpaWkyciJ9.Sxwi5wbn1DbvXsH5w9e1lw";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-87.639, 41.8954],
+  zoom: 12,
+  style: "mapbox://styles/mapbox/streets-v10"
+});
